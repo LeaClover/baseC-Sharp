d4e79f96116bb64899ac785a03992c8bf5a2c67f
LeaClover <plesen_14@mail.ru>
2023-10-13 22:40:12 +0300

    add new project hw1

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeWork1 {
+    internal class Program {
+        private static void Main(string[] args) {
+
+            if(args.Length == 3) {
+                /*Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a* b, введенные из командной строки, и выводящую результат выполнения на экран.*/
+                double result = 0;
+
+                if (args[1] == "+") {
+                    result = double.Parse(args[0]) + double.Parse(args[2]);
+                    Console.WriteLine($"{args[0]} + {args[2]} = {Math.Round(result, 2)}");
+                } else if (args[1] == "-") {
+                    result = double.Parse(args[0]) - double.Parse(args[2]);
+                    Console.WriteLine($"{args[0]} - {args[2]} = {Math.Round(result, 2)}");
+                } else if (args[1] == "*") {
+                    result = double.Parse(args[0]) * double.Parse(args[2]);
+                    Console.WriteLine($"{args[0]} * {args[2]} = {Math.Round(result, 2)}");
+                } else if (args[1] == "/") {
+                    if (args[2] != "0") {
+                        result = double.Parse(args[0]) / double.Parse(args[2]);
+                        Console.WriteLine($"{args[0]} * {args[2]} = {Math.Round(result, 2)}");
+                    } else {
+                        Console.WriteLine("Error. Division by zero");
+                    }
+
+                } else {
+                    Console.WriteLine("Error. Need view: number (+-*/) number");
+                }
+            }
+        }
+    }
+}
